c55f0f9ff000c299503088654692155d2106d308
Loganathan Thusharkanth <thusharkanth2002@gmail.com>
2025-09-29 10:14:09 +0530

    Add voice speed control

--- TTS.py
+++ TTS.py
@@ -1,6 +1,22 @@
-from RealtimeTTS import TextToAudioStream, SystemEngine, AzureEngine, ElevenlabsEngine
+from RealtimeTTS import (
+    TextToAudioStream,
+    OpenAIEngine,
+    SystemEngine,
+    AzureEngine,
+    ElevenlabsEngine,
+)
 
-engine = SystemEngine()  # replace with your TTS engine
+engine = SystemEngine()
+engine.set_voice_parameters(rate=180)
+
+
+# replace with your TTS engine
 stream = TextToAudioStream(engine)
-stream.feed("Hello world! How are you today?")
+stream.feed(
+    "Hello I'm Mint G P T, your AI assistant. Mint HRM's vision is to help companies Develop your people, scale your business. The company provides a cloud-based Human Resources Information System (HRIS) designed to enhance people enablement processes and create a joyful and efficient HR experience for businesses"
+)
 stream.play_async()
+
+
+# change the voice
+# modify the speed #finetune the stt to understand our english
